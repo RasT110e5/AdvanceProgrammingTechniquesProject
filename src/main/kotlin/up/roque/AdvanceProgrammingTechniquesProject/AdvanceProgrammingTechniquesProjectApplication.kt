30825c9c923144b4f40cda72015e3a48db31fb3a
ras <ras_t110e5@hotmail.com>
2022-11-15 17:28:41 -0300

    Added base Spring Kotlin project with all the dependencies

---
+++ src/main/kotlin/up/roque/AdvanceProgrammingTechniquesProject/AdvanceProgrammingTechniquesProjectApplication.kt
@@ -0,0 +1,11 @@
+package up.roque.AdvanceProgrammingTechniquesProject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AdvanceProgrammingTechniquesProjectApplication
+
+fun main(args: Array<String>) {
+	runApplication<AdvanceProgrammingTechniquesProjectApplication>(*args)
+}
---
+++ src/test/kotlin/up/roque/AdvanceProgrammingTechniquesProject/AdvanceProgrammingTechniquesProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package up.roque.AdvanceProgrammingTechniquesProject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AdvanceProgrammingTechniquesProjectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
